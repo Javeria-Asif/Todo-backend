0b23399f9bf80d8ef0f4079e9f2f15299b93ea36
Javeria Asif <125354714+Javeria-Asif@users.noreply.github.com>
2024-07-11 18:27:18 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const db = "mongodb+srv://javeriaasif70:Javeria@cluster0.q4axtk1.mongodb.net/To-Do"
+const cors = require('cors');
+const todomodel = require('./models/todo')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(db).then(() => {
+    console.log("connection successfull")
+}).catch((err) => console.log("no connection"))
+
+app.get('/get', (req, res) => {
+    todomodel.find()
+        .then(result => res.json(result))
+        .catch(err => res.json(err))
+})
+
+app.put("/update/:id", (req, res) => {
+    const { id } = req.params;
+    todomodel.findByIdAndUpdate({ _id: id }, { done: true })
+        .then(result => res.json(result))
+        .catch(err => res.json(err))
+})
+
+app.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    todomodel.findByIdAndDelete({ _id: id })
+        .then(result => res.json(result))
+        .catch(err => res.json(err))
+})
+
+app.post('/add', (req, res) => {
+    const task = req.body.task;
+    todomodel.create({
+        task: task
+    }).then(result => res.json(result))
+        .catch(err => res.json(err))
+})
+
+app.listen(3001, () => {
+    console.log("server is running")
+})
+
